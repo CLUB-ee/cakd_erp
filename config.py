6081237a8f22777dc43181d597512d3f17b04e6b
zivary <zivary@me.com>
2022-09-08 14:45:08 +0900

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+# config.py
+from turtle import clear
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cakd_erp',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY
+# ALGORITHMS='HS256'
