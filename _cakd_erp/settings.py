87a919a2e74b0d707348e90721975952e4857efb
Toddbear20 <107924571+Toddbear20@users.noreply.github.com>
2022-09-22 20:49:29 +0900

    그래프 구현
    셋팅파이, 템플릿,

--- _cakd_erp/settings.py
+++ _cakd_erp/settings.py
@@ -130,6 +130,10 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'template','static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
 
