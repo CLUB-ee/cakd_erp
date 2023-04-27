263c67afca882274191307ad0c394d91f8d4cb57
zivary <96277148+zivary@users.noreply.github.com>
2023-04-27 16:15:41 +0900

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'cakd7_erp',
-        'USER': 'root',
-        'PASSWORD': '12345678',  # mysql root 유저 비밀번호 입력
+        'NAME': 'DB NAME',
+        'USER': 'DB USER NAME',  # mysql 유저 ID
+        'PASSWORD': 'PASSWARD',  # mysql 유저 PW
         'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'PORT': '3306',  # mysql port
     }
 }
